b455fc4681cda19fbf7e44e56430f203b23417e0
Dane Carson <57547321+dcarson1661@users.noreply.github.com>
2023-07-31 15:29:55 -0700

    Add files via upload

--- search_widget.js
+++ search_widget.js
@@ -1,27 +1,34 @@
-
-
 require([
     "esri/widgets/Search",
-
     "esri/layers/FeatureLayer"
 ], function(
     Search,
-
     FeatureLayer
 ) {
 
     var myGeocoder = {
         url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer",
         name: "World Geocoder",
-        placeholder: "search for an address..."
     };
 
-    var search = new Search({
-        container: "searchDiv",
-        maxSuggestions: 1,
-        arcgisGeocoder: myGeocoder
 
-    });
+    var search = new Search({
+        includeDefaultSources: false,
+        locationEnabled: true,
+        container: "searchDiv",
+        sources: [{
+            locator: myGeocoder,
+            countryCode: "USA",
+            categories: ["City"],
+            singleLineFieldName: "SingleLine",
+            maxResults: 4,
+            maxSuggestions: 4,
+            suggestionsEnabled: true,
+            minSuggestCharacters: 4,
+            placeholder: " ",
+        }]
+    })
+
 
     const featureLayer = new FeatureLayer({
         url: "https://services9.arcgis.com/jyf59MjuiWfY46oy/arcgis/rest/services/ESJGeo_gdb/FeatureServer/0"
@@ -51,7 +58,7 @@ require([
                     var GEOID = results.features[0].attributes.FIPS
                     var NAME = results.features[0].attributes.NAMELSAD.replace(' town', '').replace(' city', '').replace(' City', '')
                     var STATE = results.features[0].attributes.StateAbbr
-                    window.open(baseURL + 'GEOID=' + GEOID + '&place=' + NAME + ', ' + STATE,"_self");
+                    window.open(baseURL + 'GEOID=' + GEOID + '&place=' + NAME + ', ' + STATE, "_self");
                 } else {
                     //window.open("search.html");
                     document.getElementById("alert").open = true
@@ -62,4 +69,3 @@ require([
             });
     }
 });
-
